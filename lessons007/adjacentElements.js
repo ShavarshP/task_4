fd7453912c028b07f7cb60241c6d116af81ff3d3
ShavarshP <papoyan101097@mail.ru>
2021-05-06 16:12:30 +0400

    fix: task_11

--- lessons007/adjacentElements.js
+++ lessons007/adjacentElements.js
@@ -1,15 +1,12 @@
 //task11
-
 const adjacentElements = (arr) => {
   let newArr = []
+  let max=arr[0]*arr[1]
   arr.forEach((item, i, arr2) => {
     if (i < arr2.length - 1) {
       newArr.push(item*arr2[i+1])
+      if (item*arr2[i+1] > max) max = item*arr2[i+1];
     }
   });
-  let max=newArr[0]
-  for (i = 1; i < newArr.length; ++i) {
-      if (newArr[i] > max) max = newArr[i];
-    }
   return max
 };
