910ea9ebc957388855dbfa7a00eeea3ff7124b39
ShavarshP <papoyan101097@mail.ru>
2021-05-25 14:00:10 +0400

    fix

--- lessons017/shiritoy.js
+++ lessons017/shiritoy.js
@@ -1,23 +1,28 @@
 class Shiritory {
   constructor() {
-    this.arr=[]
-    this.gameOver=[]
-    this.lastWorld=""
-    this.over=false
+    this.arr = [];
+    this.gameOver = [];
+    this.lastWorld = "";
+    this.over = false;
   }
-  play(world){
-    if (this.arr.length==0 || this.arr.filter(t=>t==world).length==0 && this.lastWorld[this.lastWorld.length-1]==world[0] && !this.over) {
-      this.lastWorld=world
-      this.arr=[...this.arr,world]
-      return "ok"
-    }else {
-      this.over=true
-      return this.arr
+  play(world) {
+    if (
+      this.arr.length == 0 ||
+      (this.arr.filter((t) => t == world).length == 0 &&
+        this.lastWorld[this.lastWorld.length - 1] == world[0] &&
+        !this.over)
+    ) {
+      this.lastWorld = world;
+      this.arr = [...this.arr, world];
+      return "ok";
+    } else {
+      this.over = true;
+      return this.arr;
     }
   }
-  gameOver(){
-    return this.over
+  gameOver() {
+    return this.over;
   }
 }
 
-const pleyer= new Shiritory()
+const pleyer = new Shiritory();
